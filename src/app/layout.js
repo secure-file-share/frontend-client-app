c3d30561b25ea16496ade778f67cef02eeda65a3
Satshree Shrestha <40155624+satshree@users.noreply.github.com>
2023-11-25 10:59:52 -0600

    updated html meta description

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "SecureFileShare",
-  description: "SecureFileShare client app",
+  description: "Share files securely with team members",
 };
 
 export default function RootLayout({ children }) {
